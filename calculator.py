36f82926aa2b9eeed2dedcf2397c671705640b3e
Rachel <rachelsuk97@gmail.com>
2021-01-20 05:19:29 +0000

    added ) at end of line 11 to fix syntax error

--- calculator.py
+++ calculator.py
@@ -8,18 +8,18 @@ while True:
     if tokens[0]=="q":
         break
     elif tokens[0]=="+":
-        print(add(int(tokens[1]), int(tokens[2]))
+        print(add(int(tokens[1]), int(tokens[2])))
     elif tokens[0]=="-":
-        print(subtract(int(tokens[1]), int(tokens[2]))
+        print(subtract(int(tokens[1]), int(tokens[2])))
     elif tokens[0]=="*":
-        print(multiply(int(tokens[1]), int(tokens[2]))
+        print(multiply(int(tokens[1]), int(tokens[2])))
     elif tokens[0]=="/":
-        print(divide(int(tokens[1]), int(tokens[2]))
+        print(divide(int(tokens[1]), int(tokens[2])))
     elif tokens[0]=="**2":
-        print(square(int(tokens[1]), int(tokens[2]))
+        print(square(int(tokens[1]), int(tokens[2])))
     elif tokens[0]=="**3":
-        print(cube(int(tokens[1]), int(tokens[2]))
+        print(cube(int(tokens[1]), int(tokens[2])))
     elif tokens[0]=="**":
-        print((power(int(tokens[1]), int(tokens[2]))
+        print(power(int(tokens[1]), int(tokens[2])))
     elif tokens[0]=="%":
-        print(remainder(int(tokens[1]), int(tokens[2]))
+        print(mod(int(tokens[1]), int(tokens[2])))
