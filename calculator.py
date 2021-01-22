522980ffe4db17e7cdfcdd243fa15e8d7cd39b85
Rachel <rachelsuk97@gmail.com>
2021-01-22 05:25:52 +0000

    made edits to allow for infinite inputs on some operators

--- calculator.py
+++ calculator.py
@@ -1,34 +1,35 @@
 """CLI application for a prefix-notation calculator."""
+from functools import reduce 
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 while True:
     user_input = input("Please include operator, first number, second number?")
     tokens = user_input.split(',')
+    tokens_float = [float(i) for i in tokens[1:]]
     if tokens[0]=="q":
         break
-
-    if len(tokens) == 3:
-        if tokens[0]=="+":
-            print(add(float(tokens[1]), float(tokens[2])))
-        elif tokens[0]=="-":
-            print(subtract(float(tokens[1]), float(tokens[2])))
-        elif tokens[0]=="*":
-            print(multiply(float(tokens[1]), float(tokens[2])))
-        elif tokens[0]=="/":
-            print(divide(float(tokens[1]), float(tokens[2])))
-        elif tokens[0]=="**":
-            print(power(float(tokens[1]), float(tokens[2])))
-        elif tokens[0]=="%":
-            print(mod(float(tokens[1]), float(tokens[2])))
-        else: 
-            print ("not a valid command")
-    elif len(tokens) == 2: 
-        if tokens[0]=="**2":
+    elif tokens[0]=="+":
+        print(add(float(reduce(add, tokens_float[:-1])),tokens_float[-1]))
+    elif tokens[0]=="-":
+        print(subtract(float(reduce(subtract, tokens_float[:-1])),tokens_float[-1]))
+    elif tokens[0]=="*":
+        print(multiply(float(reduce(multiply, tokens_float[:-1])),tokens_float[-1]))
+    elif tokens[0]=="/":
+        print(divide(float(reduce(divide, tokens_float[:-1])),tokens_float[-1]))
+    elif tokens[0]=="**":
+        print(power(float(reduce(power, tokens_float[:-1])),tokens_float[-1]))
+    elif tokens[0]=="%":
+        print(mod(float(reduce(mod, tokens_float[:-1])),tokens_float[-1]))
+    elif tokens[0]=="**2":
+        if len(tokens)==2:
             print(square(float(tokens[1])))
-        elif tokens[0]=="**3":
-             print(cube(float(tokens[1])))
-        else: 
-            print("not a valid command")
+        else:
+            print("need to provide only one value when calculating ^2")
+    elif tokens[0]=="**3":
+        if len(tokens)==2:
+            print(cube(float(tokens[1])))
+        else:
+            print("need to provide only one value when calculating ^3")
     else:
         print("not a valid command, try again")
