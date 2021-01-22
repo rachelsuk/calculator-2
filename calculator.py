647b2d003d2b0b7686eaca191de3152b7af46fa1
Rachel <rachelsuk97@gmail.com>
2021-01-22 06:41:07 +0000

    changed from prefix notation to infix notation

--- calculator.py
+++ calculator.py
@@ -13,34 +13,33 @@ input_file = open("calculator-2_input.txt")
 output_file = open("calculator-2_output.txt", "w")
 
 for line in input_file:
-    tokens = line.split(',')
-    tokens_float = [float(i) for i in tokens[1:]]
-    if tokens[0]=="q":
-        break
-    elif tokens[0]=="+":
-        result = add(my_reduce(add, tokens_float[:-1]),tokens_float[-1])
-    elif tokens[0]=="-":
-        result = subtract(my_reduce(subtract, tokens_float[:-1]),tokens_float[-1])
-    elif tokens[0]=="*":
-        result = multiply(my_reduce(multiply, tokens_float[:-1]),tokens_float[-1])
-    elif tokens[0]=="/":
-        result = divide(my_reduce(divide, tokens_float[:-1]),tokens_float[-1])
-    elif tokens[0]=="**":
-        result = power(my_reduce(power, tokens_float[:-1]),tokens_float[-1])
-    elif tokens[0]=="%":
-        result = mod(my_reduce(mod, tokens_float[:-1]),tokens_float[-1])
-    elif tokens[0]=="**2":
-        if len(tokens)==2:
-            result = square(float(tokens[1]))
-        else:
-            result = "need to provide only one value when calculating ^2"
-    elif tokens[0]=="**3":
-        if len(tokens)==2:
-            result = cube(float(tokens[1]))
-        else:
-            result = "need to provide only one value when calculating ^3"
+    tokens = line.split(' ')
+    if tokens[1] in ["+","-","*","/","**","%","**2","**3"]:
+        tokens_float = [float(i) for i in tokens[::2]]
+        if tokens[1]=="+": 
+            result = add(my_reduce(add, tokens_float[:-1]),tokens_float[-1])
+        elif tokens[1]=="-":
+            result = subtract(my_reduce(subtract, tokens_float[:-1]),tokens_float[-1])
+        elif tokens[1]=="*":
+            result = multiply(my_reduce(multiply, tokens_float[:-1]),tokens_float[-1])
+        elif tokens[1]=="/":
+            result = divide(my_reduce(divide, tokens_float[:-1]),tokens_float[-1])
+        elif tokens[1]=="**":
+            result = power(my_reduce(power, tokens_float[:-1]),tokens_float[-1])
+        elif tokens[1]=="%":
+            result = mod(my_reduce(mod, tokens_float[:-1]),tokens_float[-1])
+        elif tokens[1]=="**2":
+            if len(tokens)==2:
+                result = square(float(tokens[1]))
+            else:
+                result = "need to provide only one value when calculating ^2"
+        elif tokens[1]=="**3":
+            if len(tokens)==2:
+                result = cube(float(tokens[1]))
+            else:
+                result = "need to provide only one value when calculating ^3"
     else:
-        result = "not a valid command, try again"
+        result = "not a valid command"
 
     if type(result) == float:
         output_file.write("{:.2f}".format(result) + "\n")
