cbc5d0a957a0f8cdc09a50af442ac19d590ecbe2
Rachel <rachelsuk97@gmail.com>
2021-01-22 04:36:28 +0000

    added error messages for invalid command

--- calculator.py
+++ calculator.py
@@ -7,19 +7,28 @@ while True:
     tokens = user_input.split(',')
     if tokens[0]=="q":
         break
-    elif tokens[0]=="+":
-        print(add(float(tokens[1]), float(tokens[2])))
-    elif tokens[0]=="-":
-        print(subtract(float(tokens[1]), float(tokens[2])))
-    elif tokens[0]=="*":
-        print(multiply(float(tokens[1]), float(tokens[2])))
-    elif tokens[0]=="/":
-        print(divide(float(tokens[1]), float(tokens[2])))
-    elif tokens[0]=="**2":
-        print(square(float(tokens[1])))
-    elif tokens[0]=="**3":
-        print(cube(float(tokens[1])))
-    elif tokens[0]=="**":
-        print(power(float(tokens[1]), float(tokens[2])))
-    elif tokens[0]=="%":
-        print(mod(float(tokens[1]), float(tokens[2])))
+
+    if len(tokens) == 3:
+        if tokens[0]=="+":
+            print(add(float(tokens[1]), float(tokens[2])))
+        elif tokens[0]=="-":
+            print(subtract(float(tokens[1]), float(tokens[2])))
+        elif tokens[0]=="*":
+            print(multiply(float(tokens[1]), float(tokens[2])))
+        elif tokens[0]=="/":
+            print(divide(float(tokens[1]), float(tokens[2])))
+        elif tokens[0]=="**":
+            print(power(float(tokens[1]), float(tokens[2])))
+        elif tokens[0]=="%":
+            print(mod(float(tokens[1]), float(tokens[2])))
+        else: 
+            print ("not a valid command")
+    elif len(tokens) == 2: 
+        if tokens[0]=="**2":
+            print(square(float(tokens[1])))
+        elif tokens[0]=="**3":
+             print(cube(float(tokens[1])))
+        else: 
+            print("not a valid command")
+    else:
+        print("not a valid command, try again")
